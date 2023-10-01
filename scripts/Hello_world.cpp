bc50eb3244590b4171304d42e5581313bce6b133
Priyanshu-su30 <107869522+Priyanshu-su30@users.noreply.github.com>
2023-10-02 00:49:19 +0530

    Create Hello_world.cpp
    Added a file that prints hello world in C++

---
+++ scripts/Hello_world.cpp
@@ -0,0 +1,13 @@
+// LANGUAGE: C++
+// AUTHOR: Priyanshu Singh
+// GITHUB: https://github.com/Priyanshu-su30
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "Hello World";
+ 
+    return 0;
+}
